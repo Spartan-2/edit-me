38f5d950275de593dabfeb9359c40ce62d056866
nradhesh <136627964+nradhesh@users.noreply.github.com>
2025-05-14 19:57:52 +0530

    functionality addition

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,18 +1,8 @@
 import SidebarButton from "@/components/sidebar/sidebar-views/SidebarButton"
-import { useAppContext } from "@/context/AppContext"
-import { useSocket } from "@/context/SocketContext"
 import { useViews } from "@/context/ViewContext"
 import useResponsive from "@/hooks/useResponsive"
-import useWindowDimensions from "@/hooks/useWindowDimensions"
-import { ACTIVITY_STATE } from "@/types/app"
-import { SocketEvent } from "@/types/socket"
 import { VIEWS } from "@/types/view"
-import { IoCodeSlash } from "react-icons/io5"
-import { MdOutlineDraw } from "react-icons/md"
 import cn from "classnames"
-import { Tooltip } from 'react-tooltip'
-import { useState } from 'react'
-import { tooltipStyles } from "./tooltipStyles"
 
 function Sidebar() {
     const {
@@ -20,30 +10,8 @@ function Sidebar() {
         isSidebarOpen,
         viewComponents,
         viewIcons,
-        setIsSidebarOpen,
     } = useViews()
     const { minHeightReached } = useResponsive()
-    const { activityState, setActivityState } = useAppContext()
-    const { socket } = useSocket()
-    const { isMobile } = useWindowDimensions()
-    const [showTooltip, setShowTooltip] = useState(true)
-
-    // Commented out drawing mode toggle functionality
-    /*
-    const changeState = () => {
-        setShowTooltip(false)
-        if (activityState === ACTIVITY_STATE.CODING) {
-            setActivityState(ACTIVITY_STATE.DRAWING)
-            socket.emit(SocketEvent.REQUEST_DRAWING)
-        } else {
-            setActivityState(ACTIVITY_STATE.CODING)
-        }
-
-        if (isMobile) {
-            setIsSidebarOpen(false)
-        }
-    }
-    */
 
     return (
         <aside className="flex w-full md:h-full md:max-h-full md:min-h-full md:w-auto">
@@ -59,10 +27,6 @@ function Sidebar() {
                     viewName={VIEWS.FILES}
                     icon={viewIcons[VIEWS.FILES]}
                 />
-                {/* <SidebarButton
-                    viewName={VIEWS.CHATS}
-                    icon={viewIcons[VIEWS.CHATS]}
-                /> */}
                 <SidebarButton
                     viewName={VIEWS.COPILOT}
                     icon={viewIcons[VIEWS.COPILOT]}
@@ -71,49 +35,6 @@ function Sidebar() {
                     viewName={VIEWS.RUN}
                     icon={viewIcons[VIEWS.RUN]}
                 />
-                {/* <SidebarButton
-                    viewName={VIEWS.CLIENTS}
-                    icon={viewIcons[VIEWS.CLIENTS]}
-                /> */}
-                {/* <SidebarButton
-                    viewName={VIEWS.SETTINGS}
-                    icon={viewIcons[VIEWS.SETTINGS]}
-                /> */}
-
-                {/* Drawing mode toggle button commented out */}
-                {/*
-                <div className="flex h-fit items-center justify-center">
-                    <button
-                        className="justify-cente flex items-center  rounded p-1.5 transition-colors duration-200 ease-in-out hover:bg-[#3D404A]"
-                        onClick={changeState}
-                        onMouseEnter={() => setShowTooltip(true)}
-                        data-tooltip-id="activity-state-tooltip"
-                        data-tooltip-content={
-                            activityState === ACTIVITY_STATE.CODING
-                                ? "Switch to Drawing Mode"
-                                : "Switch to Coding Mode"
-                        }
-                    >
-                        {activityState === ACTIVITY_STATE.CODING ? (
-                            <MdOutlineDraw size={30} />
-                        ) : (
-                            <IoCodeSlash size={30} />
-                        )}
-                    </button>
-                    {showTooltip && (
-                        <Tooltip
-                            id="activity-state-tooltip"
-                            place="right"
-                            offset={15}
-                            className="!z-50"
-                            style={tooltipStyles}
-                            noArrow={false}
-                            positionStrategy="fixed"
-                            float={true}
-                        />
-                    )}
-                </div>
-                */}
             </div>
             <div
                 className="absolute left-0 top-0 z-20 w-full flex-col bg-dark md:static md:min-w-[300px]"
