e7ff454f669f66200136aeb548bd7d050be48585
nradhesh <136627964+nradhesh@users.noreply.github.com>
2025-05-21 23:48:52 +0530

    Remove legacy .eslintrc.js to fix ESLint config error in CI

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -6,19 +6,18 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:prettier/recommended"
+        "plugin:react-hooks/recommended"
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: { ecmaFeatures: { jsx: true }, ecmaVersion: 12, sourceType: "module" },
-    plugins: ["react", "@typescript-eslint", "prettier"],
+    plugins: ["react", "@typescript-eslint", "react-refresh"],
     settings: { react: { version: "detect" } },
     rules: {
         "react/react-in-jsx-scope": "off",
         "react/prop-types": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
         "@typescript-eslint/no-explicit-any": "warn",
-        "prettier/prettier": "error"
+        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
     },
     overrides: [
         { files: ["**/__mocks__/**", "**/*.mock.js", "**/*.mock.ts"], extends: ["./.eslintrc.mock.cjs"] }
